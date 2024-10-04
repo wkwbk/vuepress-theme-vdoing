9bd6296f652bd3fd065dcfbbc827eabebf47dc63
wkwbk <lisir.me@outlook.com>
2024-10-05 00:49:48 +0800

    2024年10月5日00点49分

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-push_addr="https://wkwbk:${GITHUB_TOKEN}@github.com/wkwbk/vuepress-theme-vdoing.git"
+push_addr='git@github.com:wkwbk/vuepress-theme-vdoing.git' # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=$(date '+%F %H:%M:%S')
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
 push_branch=gh-pages              # 推送的分支
 
 # 生成静态文件
-npm run build
+npm run build:win
 
 # 进入生成的文件夹
 cd $dist_path
@@ -17,8 +17,6 @@ cd $dist_path
 echo 'blog.lisir.me' >CNAME
 
 git init
-git config user.name "wkwbk"
-git config user.email "lisir.me@outlook.com"
 git add -A
 git commit -m "Site updated: $commit_info"
 git push -f $push_addr HEAD:$push_branch
